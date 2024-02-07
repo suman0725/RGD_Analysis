ac50b23949ee61d3ea3d15114c162bff70353c1f
suman0725 <92217486+suman0725@users.noreply.github.com>
2024-02-07 14:02:22 -0500

    Delete M1M directory
    pacages

--- M1M/PhiVz.C
+++
@@ -1,5 +0,0 @@
-{
-hists.Hist2D("PBANK.Vz:P.Phi*TMath::RadToDeg()",200,-20,20,200,-180,180,"P.Pid==11")->Draw("colz");
-
-hists.Save("PhiVz.root");
-}
--- M1M/ThetaVz.C
+++
@@ -1,5 +0,0 @@
-{
-hists.Hist2D("PBANK.Vz:P.Theta*TMath::RadToDeg()",200,-30,20,200,0,180,"P.Pid==11")->Draw("colz");
-
-hists.Save("ThetaVz.root");
-}
